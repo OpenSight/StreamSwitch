ecee13aacfdd66dbce053106f2012387bd81305e
jamken <zqwangjk@sina.com>
2014-11-10 23:11:56 +0800

    update some source

--- core/include/stsw_defs.h
+++ core/include/stsw_defs.h
@@ -1,8 +1,46 @@
 #ifndef STSW_DEFS_H
 #define STSW_DEFS_H
 
+#include <stdio.h>
+
+namespace stream_switch {
+    
+class StreamSource;
+class ProtoCommonPacket;
+
+typedef int (*SourceApiHandler)(StreamSource * source, ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+
+struct SourceApiHandlerEntry{
+    SourceApiHandler handler;
+    void * user_data;
+};
+
+
+enum SourceStreamState {
+  SOURCE_STREAM_STATE_CONNECTING = 0,
+  SOURCE_STREAM_STATE_OK = 1,
+  SOURCE_STREAM_STATE_ERR = -1,
+  SOURCE_STREAM_STATE_ERR_CONNECT_FAIL = -2,
+  SOURCE_STREAM_STATE_ERR_MEIDA_STOP = -3,
+  SOURCE_STREAM_STATE_ERR_TIME = -4
+};
+
+
+enum StreamPlayType{
+    STREAM_PLAY_TYPE_LIVE = 0,
+    STREAM_PLAY_TYPE_REPLAY = 1    
+};
+
+
+enum SourceSubStreamMediaType{
+    SUB_STREAM_MEIDA_TYPE_VIDEO = 0;
+    SUB_STREAM_MEIDA_TYPE_AUDIO = 1;
+    SUB_STREAM_MEIDA_TYPE_TEXT = 2;
+    SUB_STREAM_MEIDA_TYPE_PRIVATE = 3;
+}
 
 
 
+}
 
 #endif
--- core/include/stsw_stream_source.h
+++ core/include/stsw_stream_source.h
@@ -17,48 +17,100 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
+/**
+ * stsw_stream_source.h
+ *      StreamSource class header file, declare all interfaces of StreamSource.
+ * 
+ * author: jamken
+ * date: 2014-11-8
+**/ 
+
 
 #ifndef STSW_STREAM_SOURCE_H
 #define STSW_STREAM_SOURCE_H
+#include<map>
 #include<czmq.h>
 #include<pthreahd.h>
 
-namespace stream_switch {
-    class StreamSource{
-    public:
-        void init(void);
-        void uninit(void);
-        void set_stream_meta(void);
-        void get_stream_meta(void);
-        void start_api_thread(void);
-        void stop_api_thread(void);
-        void send_media_data(void);
-        void send_stream_info(void);
-        void set_stream_state(void);
-        
-        
-        void key_frame(void);
-        void get_packet_statistic(void);
-        
-    protected:
-        zsock_t * api_socket;
-        zsock_t * publish_socket;
-        pthread_mutex_t lock;
-        pthread_t * api_thread;        
-        void * api_handler_map;
-        uint32_t flags; /*bit 0: meta data ready*/        
-        void * staitistic;
-        
-        void * stream_meta;
-        uint32_t cur_bps;
-        int64_t last_frame_sec;
-        int32_t last_frame_usec;
-        int stream_state;
-        void * clientList;
+#include<stsw_defs.h>
+#include<stdint.h>
 
+
+namespace stream_switch {
+
+class ProtoClientHeartbeatReq;
+typedef std::map<int, SourceApiHandlerEntry> SourceApiHanderMap;
+struct ClientListType;
+    
+class StreamSource{
+public:
+    StreamSource();
+    virtual ~StreamSource();
+    
+    // The following methods should be invoked by clients
+    virtual int init(int argc, char* argv[]);
+    virtual void uninit();
+    
+    virtual void set_stream_meta();
+    virtual void get_stream_meta();
+    
+    virtual int start_api_thread();
+    virtual void stop_api_thread();
+    
+    virtual int send_media_data(void);
+    virtual int send_stream_info(void);
+    
+    virtual void set_stream_state(int new_state);
+    
+    virtual void register_api_handler(int op_code, SourceApiHandler handler, void * user_data);
+    virtual void unregister_api_handler(int op_code);
+    
+    
         
-               
-    }
+    // the following methods need client to override
+    // to fulfill its functions. They would be invoked
+    // by the internal api thread 
+        
+    // key_frame
+    // When the receiver request a key frame, it would be 
+    // invoked
+    virtual void key_frame(void);
+        
+    // get_packet_statistic
+    // When    
+    virtual void get_packet_statistic(uint64_t &total_packets, uint64_t &receive_packets);
+
+
+    static int static_metadata_handlertypedef(StreamSource * source, ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    static int static_key_frame_handler(StreamSource * source, ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    static int static_statistic_handler(StreamSource * source, ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    static int static_client_heartbeat_handler(StreamSource * source, ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    
+
+protected:
+    int metadata_handler(ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    int key_frame_handler(ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    int statistic_handler(ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+    int client_heartbeat_handler(ProtoCommonPacket * request, ProtoCommonPacket * reply, void * user_data);
+
+protected:
+    zsock_t * api_socket;
+    zsock_t * publish_socket;
+    pthread_mutex_t lock;
+    pthread_t * api_thread;
+    SourceApiHanderMap api_handler_map;
+    uint32_t flags; /*bit 0: meta data ready*/        
+    void * staitistic;
+        
+    void * stream_meta;
+    uint32_t cur_bps;
+    int64_t last_frame_sec;
+    int32_t last_frame_usec;
+    int stream_state;
+    ClientListType * clientList;
+    
+                         
+};
 }
 
 #endif
