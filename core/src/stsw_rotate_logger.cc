c52a39f4819ba935b3e391cfb4e21cb4f4b7b844
jamken <zqwangjk@sina.com>
2014-12-08 17:33:32 +0800

    add logger class framework

---
+++ core/src/stsw_rotate_logger.cc
@@ -0,0 +1,48 @@
+/**
+ * StreamSwitch is an extensible and scalable media stream server for 
+ * multi-protocol environment. 
+ * 
+ * Copyright (C) 2014  OpenSight (www.opensight.cn)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+/**
+ * stsw_rotate_logger.cc
+ *      RotateLogger class implementation file, define all methods of RotateLogger.
+ * 
+ * author: jamken
+ * date: 2014-12-8
+**/ 
+
+#include <stsw_rotate_logger.h>
+#include <stdint.h>
+#include <list>
+#include <string.h>
+#include <errno.h>
+
+
+#include <stsw_lock_guard.h>
+
+
+namespace stream_switch {
+
+
+
+
+}
+
+
+
+
+
