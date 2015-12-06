57480607accb7d584ad8ff6761b8953bde2b68fd
jamken <zqwangjk@sina.com>
2015-12-06 09:34:10 +0800

    test change name

--- senders/ffmpeg_sender/src/stsw_ffmpeg_muxer.cc
+++ senders/ffmpeg_sender/src/stsw_ffmpeg_muxer.cc
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
+
 /**
  * stsw_ffmpeg_muxer.cc
  *      FFmpegMuxer class implementation file, define its methods
